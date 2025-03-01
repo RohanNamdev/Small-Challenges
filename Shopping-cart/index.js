ca90c4cb0f6e1e288a3f6c9c70fc62c468fac8e4
RohanNamdev <rnamdev715@gmail.com>
2025-03-01 17:17:17 +0530

    modified the Shopping Cart Challenge

--- Shopping-cart/index.js
+++ Shopping-cart/index.js
@@ -7,10 +7,18 @@ Then find a method that will round the total to 2 decimal places.
 Example output: 73.44
 */
 
-function total(arr){
-  return arr
-  .map(e=>e.price)
-  .reduce((a,b)=>a+b).toFixed(2)
+function total(arr) {
+  //   return arr
+  //   .map(e=>e.price)
+  //   .reduce((a,b)=>a+b).toFixed(2)
+
+  /* I had try the above solution before going for the solution then modified it */
+
+  let value = arr.reduce((acc, curr) => {
+    return acc + curr.price;
+  }, 0);
+
+  return value.toFixed(2);
 }
 
 console.log(total(shoppingCart));
